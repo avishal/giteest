a54acdf410edddae9ad74a5a712488f2b203b9a9
Vishal <vbadheli@gmail.com>
2020-05-16 06:01:28 +0530

    circle commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,55 @@
+<template>
+<div id="H1lfMy235U">
+  <input class="S1kmJn2cU" value="" placeholder="placeholder" overflow="hidden" text-overflow="ellipsis"/>
+  <input class="S1bmyh2qU" value="" placeholder="placeholder" overflow="hidden" text-overflow="ellipsis"/>
+  <input class="H14mk22c8" value="" placeholder="placeholder" overflow="hidden" text-overflow="ellipsis"/>
+  <div class="rJJNy2398"></div>
+  <input class="S1NNJnh5L" value="" placeholder="placeholder" overflow="hidden" text-overflow="ellipsis"/>
+  <button class="HkYN1hn9I">Button</button>
+  <mdc-checkbox class="H1lYS1nhqU" label="MDC Checkbox" checked indeterminate></mdc-checkbox>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#H1lfMy235U {
+  --mdc-theme-primary: #673ab7;
+  --mdc-theme-secondary: #f44336;
+  --mdc-theme-background: #ffffff;
+  position: relative;
+  margin: auto;
+  background-color: #ffffff;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+.rJJNy2398 {
+  overflow: hidden;
+  border-color: rgba(0, 0, 0, 0.15);
+  border-style: solid;
+  border-width: 1px;
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  top: 55px;
+  left: 328px;
+}
+
+.HkYN1hn9I {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  position: absolute;
+  width: 200px;
+  height: 40px;
+  top: 224px;
+  left: 4px;
+}
+
+</style>
